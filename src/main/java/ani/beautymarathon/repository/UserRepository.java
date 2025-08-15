44b3d2d5edfe83c7ba5113084258b13255ce238b
janesvami <157762726+janesvami@users.noreply.github.com>
2025-08-15 19:12:39 +0300

    delete unused methods, add logs to gitignore

--- src/main/java/ani/beautymarathon/repository/UserRepository.java
+++ src/main/java/ani/beautymarathon/repository/UserRepository.java
@@ -4,9 +4,6 @@ import ani.beautymarathon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
